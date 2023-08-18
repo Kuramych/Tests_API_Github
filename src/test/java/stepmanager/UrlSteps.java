387be97184ec5e4db9057cbc84d427be66d6b01d
My Name <vvmelnichenko@miem.hse.ru>
2023-08-18 17:30:16 +0300

    Новый dataprovider для теста testCompareInfoFromRepositories

--- src/test/java/stepmanager/UrlSteps.java
+++ src/test/java/stepmanager/UrlSteps.java
@@ -7,11 +7,9 @@ import model.Repository;
 
 public class UrlSteps {
 
-    @Step("Ссылка repositoryUrl парсится и приводится к типу class model.Repository")
+    @Step("Ссылка {repositoryUrl} парсится и приводится к типу class model.Repository")
     public Repository getRepository(String repositoryUrl) {
-        String apiBaseUrl = "https://api.github.com";
-        String apiUrl = String.format("%s/repos/%s", apiBaseUrl, repositoryUrl);
-        String[] parts = apiUrl.split("/");
+        String[] parts = repositoryUrl.split("/");
         String name = parts[5];
         String fullname = parts[4] + "/" + parts[5];
         Repository repositoryFromSplitUrl = new Repository().withName(name).withFullName(fullname);
--- src/test/java/tests/SelenideApiTests.java
+++ src/test/java/tests/SelenideApiTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.annotations.DataProvider;
 import stepmanager.ApiSteps;
 import appmanager.PropertiesHelper;
 import stepmanager.UrlSteps;
@@ -13,46 +14,43 @@ import org.testng.annotations.Test;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Listeners(AllureTestNg.class)
 @Feature("Tests for Selenide with Api GitHub")
 public class SelenideApiTests {
 
-
     public PropertiesHelper propertiesHelper;
     public ApiSteps apiSteps = new ApiSteps();
     public UrlSteps urlSteps = new UrlSteps();
 
-
-
     public SelenideApiTests() throws IOException {
         propertiesHelper = PropertiesHelper.getInstance();
     }
-/*
-    @DataProvider
-    public Iterator<Object[]> validUrls() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/urls.csv")))) {
-            String line = reader.readLine();
-            while (line != null) {
-                String[] split = line.split(";");
-                list.add(split);
-                line = reader.readLine();
-            }
-            return list.iterator();
+
+    @DataProvider(name="repositoryList")
+    public Iterator<Object[]> repositoryList() {
+        List<Repository> data = new ArrayList<>();
+        data = apiSteps.getRepositoriesFromResponse("selenide");
+        List<Object[]> list = new ArrayList<>();
+        for (Repository repository : data) {
+            list.add(new Object[] {repository});
         }
+        return list.iterator();
     }
- */
+
 
     @Test(description = "Получение всех репозиториев, расположенных на заданном репозитории.")
     public void testGetRepositoriesListFromSelenide() {
         apiSteps.getRepositoriesFromResponse("selenide");
     }
 
-    @Test(dataProvider = "validUrls", description = "Отправка API запроса из файла src/test/resources/urls.csv. Сравнивнение API ответа с реальной информацией, полученной из ссылки путем сплита.")
-    public void testCompareInfoFromRepositories(String repositoryUrl)  {
-        Repository repositoryFromResponse = apiSteps.getRepositoryFromResponse(repositoryUrl, Repository.class);
+    @Test(dataProvider = "repositoryList", description = "Проверка, что параметры name и full_name совпадает с действительностью.")
+    public void testCompareInfoFromRepositories(Repository repository)  {
+        String repositoryUrl = repository.getUrl();
         Repository repositoryFromSplitUrl = urlSteps.getRepository(repositoryUrl);
-        Assert.assertEquals(repositoryFromResponse, repositoryFromSplitUrl);
+        Assert.assertEquals(repository, repositoryFromSplitUrl);
     }
 }
